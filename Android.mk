26f6ad518780d656a813b19590a5ac00f33cf03f
Giuseppe Barba <giuseppe.barba@st.com>
2015-07-15 14:36:22 +0200

    Enable LSM6DS3 and LIS3MDL without any additional lib
    Signed-off-by: Giuseppe Barba <giuseppe.barba@st.com>

--- Android.mk
+++ Android.mk
@@ -74,8 +74,8 @@ endef
 # E.g.: to enable LSM6DS0 + LIS3MDL sensor                                     #
 #                ENABLED_SENSORS := LSM6DS0 LIS3MDL                            #
 ################################################################################
-ENABLED_SENSORS := LSM303C  L3GD20
-ENABLED_MODULES := SENSOR_FUSION GEOMAG_FUSION MAGCALIB GBIAS
+ENABLED_SENSORS := LSM6DS3  LIS3MDL
+ENABLED_MODULES :=
 
 LOCAL_ARM_MODE := arm
 LOCAL_PRELINK_MODULE := false
