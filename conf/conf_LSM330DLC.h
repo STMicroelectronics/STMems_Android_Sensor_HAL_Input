858cb2aa1dd160daba3e6effbb2a0465502a7c8d
Lorenzo Bianconi <lorenzo.bianconi@st.com>
2016-05-11 16:13:02 +0200

    lsm330dlc: use msc input events insted of abs ones

--- conf/conf_LSM330DLC.h
+++ conf/conf_LSM330DLC.h
@@ -64,19 +64,19 @@
 /*****************************************************************************/
 /* EVENT TYPE */
 /*****************************************************************************/
-#define EVENT_TYPE_ACCEL		EV_ABS
-#define EVENT_TYPE_GYRO		EV_ABS
+#define EVENT_TYPE_ACCEL		EV_MSC
+#define EVENT_TYPE_GYRO			EV_MSC
 
 /* Event Type in accelerometer sensor: see input_set_abs_params() function in your input driver */
-#define EVENT_TYPE_ACCEL_X		ABS_X
-#define EVENT_TYPE_ACCEL_Y		ABS_Y
-#define EVENT_TYPE_ACCEL_Z		ABS_Z
+#define EVENT_TYPE_ACCEL_X		MSC_SERIAL
+#define EVENT_TYPE_ACCEL_Y		MSC_PULSELED
+#define EVENT_TYPE_ACCEL_Z		MSC_GESTURE
 
 
 /* Event Type in gyroscope sensor: see input_set_abs_params() function in your input driver */
-#define EVENT_TYPE_GYRO_X		ABS_X
-#define EVENT_TYPE_GYRO_Y		ABS_Y
-#define EVENT_TYPE_GYRO_Z		ABS_Z
+#define EVENT_TYPE_GYRO_X		MSC_SERIAL
+#define EVENT_TYPE_GYRO_Y		MSC_PULSELED
+#define EVENT_TYPE_GYRO_Z		MSC_GESTURE
 
 /*****************************************************************************/
 /* AXIS MAPPING */
