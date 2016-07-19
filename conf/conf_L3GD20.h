fe868db18c30ab51f0c360bfad6a0ab3e39cd551
Mario Tesi <mario.tesi@st.com>
2016-07-19 12:11:26 +0200

    L3GD20 L3GD20H timestamp fot HiFi cts

--- conf/conf_L3GD20.h
+++ conf/conf_L3GD20.h
@@ -54,6 +54,8 @@
 /* Event Type in gyroscope sensor: see input_set_abs_params() function in your input driver */
 #define EVENT_TYPE_GYRO		EV_MSC
 
+#define EVENT_TYPE_TIME_MSB		MSC_SCAN
+#define EVENT_TYPE_TIME_LSB		MSC_MAX
 #define EVENT_TYPE_GYRO_X		MSC_SERIAL
 #define EVENT_TYPE_GYRO_Y		MSC_PULSELED
 #define EVENT_TYPE_GYRO_Z		MSC_GESTURE
@@ -113,5 +115,7 @@ static short matrix_gyr[3][3] = {
 #define CONVERT_GYRO_Y			(CONVERT_GYRO)
 #define CONVERT_GYRO_Z			(CONVERT_GYRO)
 
+#define GYRO_EVENT_HAS_TIMESTAMP		1
+#define INPUT_EVENT_HAS_TIMESTAMP		1
 
 #endif	/*	CONFIGURATION_L3GD20_H	*/
